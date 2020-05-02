b8b15cf884acf41e28bf8a3c9f216fda6b2943d6
Sergio Tabares <alu0101124896@ull.edu.es>
2020-05-02 19:39:29 +0000

    Board class structure

---
+++ src/board.js
@@ -0,0 +1,31 @@
+/**
+ * @file board.js
+ * @author Sergio Tabares Hernández <alu0101124896@ull.edu.es>
+ * @since Spring 2020
+ * @summary University of La Laguna
+ * @summary Computer Science - Interactive Aplication Programing
+ * @description This program implements a board class for the chess.
+ */
+
+"use strict";
+
+/**
+ * @description Class representing an board
+ *
+ * @class Board
+ */
+class Board {
+
+  /**
+   * @description Constructor that creates an instance of a board.
+   *
+   * @memberof Board
+   */
+  constructor() {
+  }
+}
+
+/* istanbul ignore next */
+if (typeof exports !== 'undefined') { // Execution in node
+  exports.Board = Board;
+}
