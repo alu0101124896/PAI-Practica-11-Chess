0eb468c81c42532fb753b828cb49f80e09e107f8
Sergio Tabares <alu0101124896@ull.edu.es>
2020-05-02 19:41:29 +0000

    Board yCoord property green

--- src/board.js
+++ src/board.js
@@ -21,8 +21,9 @@ class Board {
    *
    * @memberof Board
    */
-  constructor(xCoord = 0) {
+  constructor(xCoord = 0, yCoord = 0) {
     this.xCoord = xCoord;
+    this.yCoord = yCoord;
   }
 }
 
