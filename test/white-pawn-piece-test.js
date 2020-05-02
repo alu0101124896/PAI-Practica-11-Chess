b42c243f6e879e39f1384d64013c76eb59c7de4d
Sergio Tabares <alu0101124896@ull.edu.es>
2020-05-02 19:32:24 +0000

    White Pawn Piece inherits all properties correctly

---
+++ test/white-pawn-piece-test.js
@@ -0,0 +1,70 @@
+/**
+ * @file white-pawn-piece-test.js
+ * @author Sergio Tabares Hernández <alu0101124896@ull.edu.es>
+ * @since Spring 2020
+ * @summary University of La Laguna
+ * @summary Computer Science - Interactive Aplication Programing
+ * @description This program implements the tests for the White Pawn Piece
+ *  class
+ */
+
+"use strict";
+
+let expectOnPieceTest;
+let WhitePawnPieceOnWhitePawnPieceTest;
+if (typeof require !== 'undefined') { // Execution in node
+  expectOnPieceTest = require('chai').expect;
+  WhitePawnPieceOnWhitePawnPieceTest = require('../src/white-pawn-piece.js').WhitePawnPiece;
+} else { // Execution in browser
+  expectOnPieceTest = expect;
+  WhitePawnPieceOnWhitePawnPieceTest = WhitePawnPiece;
+}
+
+describe('White Pawn Piece Class', () => {
+  describe('Default inherited properties', () => {
+    const MY_WHITE_PAWN_PIECE = new WhitePawnPieceOnWhitePawnPieceTest();
+
+    it('White Pawn Piece has a xCoord', () => {
+      expectOnPieceTest(MY_WHITE_PAWN_PIECE).to.have.property('xCoord');
+      expectOnPieceTest(MY_WHITE_PAWN_PIECE.xCoord).to.be.a('number');
+    });
+
+    it('White Pawn Piece has a yCoord', () => {
+      expectOnPieceTest(MY_WHITE_PAWN_PIECE).to.have.property('yCoord');
+      expectOnPieceTest(MY_WHITE_PAWN_PIECE.yCoord).to.be.a('number');
+    });
+
+    it('White Pawn Piece has a size', () => {
+      expectOnPieceTest(MY_WHITE_PAWN_PIECE).to.have.property('size');
+      expectOnPieceTest(MY_WHITE_PAWN_PIECE.size).to.be.a('number');
+    });
+
+    it('Default xCoord is 0', () => {
+      expectOnPieceTest(MY_WHITE_PAWN_PIECE.xCoord).to.be.equal(0);
+    });
+
+    it('Default yCoord is 0', () => {
+      expectOnPieceTest(MY_WHITE_PAWN_PIECE.yCoord).to.be.equal(0);
+    });
+
+    it('Default size is 10', () => {
+      expectOnPieceTest(MY_WHITE_PAWN_PIECE.size).to.be.equal(10);
+    });
+  });
+
+  describe('Non default property values', () => {
+    const MY_WHITE_PAWN_PIECE = new WhitePawnPieceOnWhitePawnPieceTest(5, 10, 15);
+
+    it('Modifies default xCoord correctly', () => {
+      expectOnPieceTest(MY_WHITE_PAWN_PIECE.xCoord).to.be.equal(5);
+    });
+
+    it('Modifies default yCoord correctly', () => {
+      expectOnPieceTest(MY_WHITE_PAWN_PIECE.yCoord).to.be.equal(10);
+    });
+
+    it('Modifies default size correctly', () => {
+      expectOnPieceTest(MY_WHITE_PAWN_PIECE.size).to.be.equal(15);
+    });
+  });
+});
